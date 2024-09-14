67b539486a676b2437147bb77e9681f1ac9d3e99
Franklin Mamani Apaza <franklinmamaniapaza88@gmail.com>
2024-09-13 23:33:42 -0500

    Se agrego la opcion Intercambio directo por la derecha

--- interDirecto.cpp
+++ interDirecto.cpp
@@ -1,5 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void interdirder(int arr[], int a){
+	for(int i=0; i<a-1; i++){
+		for(int j=i+1; j<a; j++){
+			if(arr[i]>arr[j]){
+				int aux;
+				aux=arr[i];
+				arr[i]=arr[j];
+				arr[j]=aux;
+			}
+		}
+	}
+	cout<<endl;
+	for(int i=0; i<a; i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
 int main (){
 	
 	int n, arr[100], op;
@@ -12,16 +31,19 @@ int main (){
 	}
 	
 	do{
+		cout<<endl;
 		cout<<"1) Intercambio directo por la derecha. "<<endl;
     	cout<<"2) Intercambio directo por la izquierda. "<<endl; 
         cout<<"3) Intercambio directo con senal. "<<endl;
         cout<<"4) Intercambio directo bidireccional. "<<endl;
         cout<<"5) Salir del programa. "<<endl;
+        cout<<endl;
+        cout<<"Ingrese la opcion que desea: ";
         cin>>op;
         
         switch(op){
         	case 1:
-        		
+        		interdirder(arr, n);
         		break;
         	case 2:
 			
@@ -40,3 +62,5 @@ int main (){
 	while(op!=0);
 	return 0;
 }
+
+
